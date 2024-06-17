9b2ad543fe8aae8ee348e5b95fd00133f4269862
MJLeech <mikeleechjr222@gmail.com>
2024-06-17 16:45:04 -0400

    created application minus licence section

--- assets/generateMarkdown.js
+++ assets/generateMarkdown.js
@@ -1,20 +1,63 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
-
+// if licence.name = 'MIT'
+// return 
+// else licence.name = 'apache'
+// return
+// if else 
+// return
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {
+// if{licence.name = 'MIT'}
+// return ""
+// else{licence.name = 'apache'}
+// return
+// else{licence.name = 'none'}
+// return ""
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+    return `
+# ${data.title}
+ ## Description
+  ${data.description}
+ ## Table of contents
+* Installation
+* Usage
+* Licence
+* Contributing
+* Questions
+* Testing ##Installation
+${data.installation}
+## Usage
+   ${data.usage}   
+## Licence
+     ${data.licence}
+## Contributing
+   ${data.contributing}
+## Testing
+${data.tests}
+## Questions
+ email:${data.email}
+github:${data.github}
 
 `;
 }
 
 module.exports = generateMarkdown;
+// title
+// Description
+//  Table of Contents
+//   Installation 
+//   Usage
+// License, 
+// Contributing
+// ,
+//  Tests
+//  , and Questions
--- assets/index.js
+++ assets/index.js
@@ -1,23 +1,70 @@
 const inquirer = require('inquirer');
+const fs = require('fs')
+const generateMarkdown = require("./generateMarkdown");
+const { default: Choices } = require('inquirer/lib/objects/choices');
 
-inquirer
-  .prompt([
-    {
-      message: 'Press ENTER to continue...',
-      name: 'enterKey'
-    }
-  ]);
+const questions = [
+  {
+    name:"title",
+    message: 'What is the title for your readme?',
+    default: 'Readme'
+  },
+  {
+    name: 'description',
+    message: 'please enter the description of your readme',
+  },
+  {
+    name: 'installation',
+    message: 'please enter instructions on installing your application',
+  },
+  {
+    name: 'usage',
+    message: 'please describe the usage of your application',
+    
+  },
+  {
+    type: 'list',
+    name: 'licence',
+    message: 'please choose a licence',
+    choices: ['None','MIT','apache'],
+    
+  },
+  {
+    name: 'contributing',
+    message: 'please enter instructions for contributing to this project',
+    
+  },
+  {
+    name: 'tests',
+    message: 'what command should be run to test this application?',
+    default: 'npm i'
+  },
+  {
+    name: 'email',
+    message: 'What is your email?',
+    
+  },
+  {
+    name: 'github',
+    message: 'What is your github username?',
+    
+  },
+];
 
-  // TODO: Include packages needed for this application
+function writeToFile(data) {
+  fs.writeFile('README.md',data, (err) =>
+    err ? console.error(err) : console.log('Generating Readme')
+  );
+}
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+  .prompt(questions)
+  .then(answers => {
+    writeToFile(generateMarkdown(answers))
+    console.log(generateMarkdown(answers))
+  });
+}
 
 // Function call to initialize app
 init();
